ac48d2b78713a56bb42cd34e2c0f3ee96836d059
ArturoPedraza1 <125940774+ArturoPedraza1@users.noreply.github.com>
2023-05-01 21:17:47 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,69 @@
+import ico.fes.Caja;
+import ico.fes.CajaSinGenericos;
+import ico.fes.Comida;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Caja<String> miCaja = new Caja<>(new String[10]);
+        miCaja.add(0, "Hola");
+        miCaja.add(1, "Adios");
+        miCaja.add(2, "Hi");
+        System.out.println(miCaja);
+
+        String tem = miCaja.get(0);
+
+        System.out.println("---------");
+        CajaSinGenericos cajaSin = new CajaSinGenericos(new Object[10]);
+        cajaSin.add(0, "Hola");
+        cajaSin.add(1, "adios");
+        cajaSin.add(2, "Hi");
+        System.out.println(cajaSin);
+        String tem2 = (String) cajaSin.get(0);
+
+        System.out.println("-----comida----");
+        Caja<Comida> cajaComida = new Caja<>(new Comida[10]);
+        cajaComida.add(0, new Comida("Yogurth", true));
+        System.out.println(cajaComida.get(0));
+        System.out.println("---------");
+
+        ArrayList<Comida> comidas = new ArrayList<>();
+
+        comidas.add(new Comida("Zanahoria", false));
+        comidas.add(new Comida("Zanahoria", false));
+        comidas.add(new Comida("Zanahoria", false));
+        comidas.add(new Comida("Zanahoria", false));
+        comidas.add(new Comida("Zanahoria", false));
+        comidas.add(new Comida("Zanahoria", false));
+        comidas.add(new Comida("Zanahoria", false));
+        comidas.add(new Comida("Zanahoria", false));
+        comidas.add(new Comida("Zanahoria", false));
+        comidas.add(new Comida("Zanahoria", false));
+        comidas.add(new Comida("Zanahoria", false));
+        comidas.add(new Comida("Zanahoria", false));
+        comidas.add(new Comida("Zanahoria", false));
+        comidas.add(new Comida("Manzana", false));
+
+        System.out.println(comidas.size());
+        System.out.println(comidas.get(13));
+
+        ArrayList<Comida> comidas2 = new ArrayList<>();
+        comidas2.add(new Comida("Zanahoria", false));
+        comidas2.add(new Comida("Yogurt", false));
+        comidas2.add(new Comida("Danonino", true));
+        comidas2.add(new Comida("Manzana", false));
+        comidas2.add(new Comida("Bistec", false));
+
+        System.out.println(comidas2.get(3));
+        comidas2.set(3, new Comida("Fresa", false));
+        comidas2.add(3, new Comida("Naranja", false));
+
+        System.out.println("-----final----");
+        for (Comida comida : comidas2
+        )
+            System.out.println(comidas2);
+        {
+        }
+    }
+}
